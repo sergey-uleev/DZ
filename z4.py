ccaf8d71b6cc3cd94911f86f8062eb79428e9178
Sergey <mazi77@bk.ru>
2022-06-30 22:04:12 +0500

    z4

--- z4.py
+++ z4.py
@@ -1,1 +1,9 @@
-print('Hello Python')
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+p1 = Point(3, 4)
+p2 = Point(3, 4)
+print(hash(p1), hash(p2), sep='\n')
+print(p1 == p2)
